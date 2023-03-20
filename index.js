124710da69b8c6e0d253eca2f7328e50055c3771
Adeyemo Peter <45455927+Lammy125@users.noreply.github.com>
2023-03-20 09:31:50 +0100

    second challange

---
+++ index.js
@@ -0,0 +1,7 @@
+const { sum, calculator, subtract } = require("./math");
+
+// calculator.multiply();
+
+console.log(sum(4, 5));
+console.log(calculator.multiply(4, 5));
+console.log(subtract(4, 5));
---
+++ math.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+
+const calculator = {
+  multiply: (p, q) => {
+    return p * q;
+  },
+};
+
+const subtract = (a, b) => a - b;
+
+module.exports = { sum, calculator, subtract };
